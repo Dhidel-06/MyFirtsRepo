d41cbf5f80eb9b81df3f59f23697d9e46a116efb
Dhidel <dhidelarevalo123@gmail.com>
2025-07-29 09:30:33 -0600

    intento de cajero

---
+++ Cajero.py
@@ -0,0 +1,10 @@
+# debe de ser un cajero
+
+Saldo = 0
+User = "djosorio"
+Password = 123
+
+print ("Bienvenido al mejor cajero del mundo")
+Contraseña_user = int("¿Cual es tu contraseña?")
+if Contraseña_user == User:
+    print("Entraste")
